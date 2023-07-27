e6edb92ce6e125c3833ee0348a81a008ba15d622
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-27 20:28:32 +0300

    upgrade

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,21 +1,23 @@
-const url = 'https://api.thecatapi.com/v1';
-const api_key = "live_ovszj7lWYxeV3KftLBnEE4ByIkdCvSjwlnWzAVvGLn0Mi8aXftT3udNvA6XurFkN";
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = "live_ovszj7lWYxeV3KftLBnEE4ByIkdCvSjwlnWzAVvGLn0Mi8aXftT3udNvA6XurFkN";
 
 export function fetchBreeds() {
-    return fetch(`${url}/breeds?api_key=${api_key}`)
+    const ENDPOIND = '/breeds';
+    return fetch(`${BASE_URL}${ENDPOIND}?api_key=${API_KEY}`)
     .then(response => {
         if (!response.ok) {
-            throw new Error(response.status);
+            throw new Error(response.statusText);
         }
         return response.json();
     });
 };
 
 export function fetchCatByBreed(breedId) {
-    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+    const ENDPOIND = '/images/search';
+    return fetch(`${BASE_URL}${ENDPOIND}?api_key=${API_KEY}&breed_ids=${breedId}`)
     .then(response => {
         if(!response.ok) {
-            throw new Error(response.status);
+            throw new Error(response.statusText);
         }
         return response.json();
     });
