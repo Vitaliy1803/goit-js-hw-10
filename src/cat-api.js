18ae677628267fc42c71e135250059c619ff7588
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-08-09 11:00:25 +0300

    async/await

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,37 +2,34 @@ import axios from "axios";
 const BASE_URL = 'https://api.thecatapi.com/v1';
 axios.defaults.headers.common["x-api-key"] = "live_ovszj7lWYxeV3KftLBnEE4ByIkdCvSjwlnWzAVvGLn0Mi8aXftT3udNvA6XurFkN";
 
-//const API_KEY = "live_ovszj7lWYxeV3KftLBnEE4ByIkdCvSjwlnWzAVvGLn0Mi8aXftT3udNvA6XurFkN";
+export {fetchBreeds, fetchCatByBreed};
 
-export function fetchBreeds() {
+
+async function fetchBreeds(){
+    // console.log('start');
+
     const ENDPOIND = '/breeds';
 
-    return axios.get(`${BASE_URL}${ENDPOIND}`)
-    .then(response => response.data);
+    try {
+    const response = await axios.get(`${BASE_URL}${ENDPOIND}`)
 
-    // return fetch(`${BASE_URL}${ENDPOIND}?api_key=${API_KEY}`)
-    // .then(response => {
-    //     if (!response.ok) {
-    //         throw new Error(response.statusText);
-    //     }
-    //     return response.json();
-    // });
-};
+    // console.log(response);
+    // console.log('end');
+    return response.data;
+    
+    } catch (error) {
+        throw new Error(response.statusText);
+    }
 
-export function fetchCatByBreed(breedId) {
+}
+
+async function fetchCatByBreed (breedId){
     const ENDPOIND = '/images/search';
 
-    return axios
-    .get(`${BASE_URL}${ENDPOIND}?breed_ids=${breedId}`)
-    .then(response => {
+    try {
+        const response = await axios.get(`${BASE_URL}${ENDPOIND}?breed_ids=${breedId}`)
         return response.data;
-    })
-    // return fetch(`${BASE_URL}${ENDPOIND}?api_key=${API_KEY}&breed_ids=${breedId}`)
-    // .then(response => {
-    //     if(!response.ok) {
-    //         throw new Error(response.statusText);
-    //     }
-    //     return response.json();
-    // });
-};
+    }
+}
+
 
